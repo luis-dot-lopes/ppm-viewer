2f068c40bfb9f037f365a67658051d94ffe828f2
luis-dot-lopes <luisantonio20054@gmail.com>
2023-03-29 20:35:25 -0300

    Fixed some bugs

--- render.py
+++ render.py
@@ -4,17 +4,18 @@ import sys
 width, height = 0, 0
 pixels = []
 
-if sys.argc < 2:
+if len(sys.argv) < 2:
     print("Error: no file to display")
     sys.exit(1)
 
 with open(sys.argv[1], "rb") as file:
-    filetype, dimensions, pixels = file.readlines()
+    lines = file.readlines()
+    filetype, dimensions = lines[:2]
     width, height = map(int, dimensions.decode('utf-8')[:-1].split()[:-1])
-    pixels = [int(pixel) for pixel in pixels]
+    pixels = [int(pixel) for pixels in lines[2:] for pixel in pixels]
     pixels = [tuple(pixels[i:i + 3]) for i in range(0, len(pixels), 3)]
 
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((width, height))
 imagesurface = pygame.Surface((width, height))
 imagepixels = pygame.PixelArray(imagesurface)
 for y in range(height):
@@ -31,7 +32,5 @@ while running:
             running = False
 
     screen.fill((0, 0, 0))
-    screen.blit(imagesurface, (100, 100))
-    pygame.draw.rect(screen, (255, 0, 0),
-                     pygame.rect.Rect(100, 100, width, height), 1)
+    screen.blit(imagesurface, (0, 0))
     pygame.display.flip()
